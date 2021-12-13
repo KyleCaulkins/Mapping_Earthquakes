3a0f070716ca64b1a42d0f89cfa69f9d8e9275e0
KyleCaulkins <90616094+KyleCaulkins@users.noreply.github.com>
2021-12-13 13:10:30 -0600

    Add files via upload

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -38,5 +38,11 @@ let torontoData = "https://raw.githubusercontent.com/KyleCaulkins/Mapping_Earthq
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+L.geoJSON(data, {
+  color:"#ffffa1",
+  weight: 2,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " +feature.properties.dst + "</h3>");
+  }
+}).addTo(map);
 });
